feecb69c6d9b230b1c7b162d2c430b36373f272c
dakotaotserver-glitch <dakotaotserver@gmail.com>
2026-02-12 17:45:17 -0300

    Fix duplicate entry for paralysis deflection

--- src/utils/tools.cpp
+++ src/utils/tools.cpp
@@ -887,7 +887,7 @@ const ImbuementTypeNames imbuementTypeNames = {
 	{ "skillboost magic level", IMBUEMENT_SKILLBOOST_MAGIC_LEVEL },
 	{ "increase capacity", IMBUEMENT_INCREASE_CAPACITY },
 	{ "skillboost fist", IMBUEMENT_SKILLBOOST_FIST },
-	{ "paralysis deflection", IMBUEMENT_PARALYSIS_DEFLECTION }
+	{ "paralysis deflection", IMBUEMENT_PARALYSIS_DEFLECTION },
 };
 /**
  * @Deprecated
